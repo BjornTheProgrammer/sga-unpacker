90fabfff15e082ae73f0eff587439f09e3c0823b
Bjorn Beishline <75190918+BjornTheProgrammer@users.noreply.github.com>
2025-04-11 11:20:26 -0700

    Make cli interface

--- sga-unpacker-cli/src/main.rs
+++ sga-unpacker-cli/src/main.rs
@@ -1,8 +1,25 @@
 use anyhow::Result;
 use sga::extract_all;
 
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Input file path
+    input: PathBuf,
+
+    /// Output folder path
+    #[arg(short, long, value_name = "FILE")]
+    output: PathBuf,
+}
+
 fn main() -> Result<()> {
-    extract_all("./test/ArtJapanese.sga", "./test/out2")?;
+    let cli = Cli::parse();
+    extract_all(cli.input, cli.output)?;
+
 
     Ok(())
 }
